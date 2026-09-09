5ab89be2cc58d04cf1b2cb21884e1a52ce552f21
aktilekkalchoroev <aktilekkalchoroev@gmail.com>
2026-09-09 12:15:30 +0600

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,6 +1,13 @@
 import os
+import subprocess
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# Запускаем бота в отдельном фоновом процессе ОС
+if not os.environ.get('BOT_STARTED'):
+    os.environ['BOT_STARTED'] = 'true'
+    subprocess.Popen([sys.executable, 'bot/main.py'])
 
 application = get_wsgi_application()
