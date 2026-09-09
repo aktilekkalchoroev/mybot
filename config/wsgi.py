be22bbaaaa4f0bfd6f8d075d5b12dcddd3719487
aktilekkalchoroev <aktilekkalchoroev@gmail.com>
2026-09-09 11:51:36 +0600

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,11 +1,6 @@
 import os
-import subprocess
-import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-# Автоматический запуск Telegram-бота в фоновом режиме
-subprocess.Popen([sys.executable, 'bot/main.py'])
 
 application = get_wsgi_application()
